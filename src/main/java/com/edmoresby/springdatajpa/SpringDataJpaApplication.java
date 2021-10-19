d21e437d1fa42310b3609629be365a30e539cb41
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-10-19 22:21:24 +0800

    implements single table - student

--- src/main/java/com/edmoresby/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/edmoresby/springdatajpa/SpringDataJpaApplication.java
@@ -1,7 +1,15 @@
 package com.edmoresby.springdatajpa;
 
+import com.edmoresby.springdatajpa.student.Student;
+import com.edmoresby.springdatajpa.student.StudentRepository;
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
@@ -9,5 +17,29 @@ public class SpringDataJpaApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return  args -> {
+            List<Student> students = generateRandomStudents(20);
+            studentRepository.saveAll(students);
+            studentRepository.findAll().forEach(System.out::println);
+        };
+    }
+
+    private  List<Student> generateRandomStudents(Integer amount){
+        List<Student> students = new ArrayList<>();
+        Faker faker = new Faker();
+        for (int i = 0; i < amount; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s%s@gmail.com", firstName, lastName);
+            Integer age = faker.number().numberBetween(18, 60);
+            students.add(new Student(firstName, lastName, email, age));
+        }
+
+        return students;
+    }
+
 
 }
